ef57f2833f9de5334835359cca8c95b08999f9df
asifhassan8231 <asifhassan8231@gmail.com>
2021-11-10 22:58:32 +0600

    date problem

--- src/Dashboard/Appointments/Appointments.js
+++ src/Dashboard/Appointments/Appointments.js
@@ -7,7 +7,7 @@ const Appointments = ({ date }) => {
     const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
-        const url = `https://salty-brook-32025.herokuapp.com/appointments?email=${user.email}&date=${date}`;
+        const url = `https://salty-brook-32025.herokuapp.com/appointments?email=${user.email}&date=${date.toLocaleDateString()}`;
         fetch(url, {
             headers: {
                 'authorization': `Bearer ${token}`
